8ccbc29d13ee7363d8f87abee144771d3da87ae8
Haris-Zafar <i201885@nu.edu.pk>
2025-03-12 08:08:13 +0500

    set simple routes, controlers, middlewares

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,16 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import mongoose from 'mongoose';
+const express = require('express')
+const dotenv = require('dotenv').config()
+const { errorHandler } = require('./middleware/errorMiddleware')
+const mongoose = require('mongoose')
 
-dotenv.config();
 const port =  process.env.PORT || 3000;
 const app = express();
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
+
+app.use('/api/goals', require('./routes/goalRoutes'))
+app.use(errorHandler)
+
 mongoose.connect('mongodb://localhost:27017/');
 
 app.listen(port, () => console.log(`server is running on ${port}`));
